8ab3309576d31997c0ec81165a4dd343b8a97aa2
Amber Costley <amber.costley@gmail.com>
2019-02-22 16:39:45 -0800

    Changed type

--- src/http/get-index/index.js
+++ src/http/get-index/index.js
@@ -16,7 +16,7 @@ let body = `
   <body>
 
     <h1 class="center-text">
-      <!-- ↓ Change "Hello world!" to something else and head on back to Begin! -->
+      <!-- ↓ Change "Hey now hey now nah now!" to something else and head on back to Begin! -->
       Hiya hiya!
     </h1>
 
