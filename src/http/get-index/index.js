41889ec77703960af58ec048cdf7ab9549d999d1
Amber Costley <amber.costley@gmail.com>
2019-03-12 17:25:21 -0700

    Update index.js

--- src/http/get-index/index.js
+++ src/http/get-index/index.js
@@ -5,26 +5,88 @@
 
 // TODO: modify the body object!
 let body = `
-<!doctype html>
-<html lang=en>
-  <head>
-    <meta charset=utf-8>
-    <title>Hi!</title>
-    <link rel="stylesheet" href="https://static.begin.app/starter/default.css">
-    <link href="data:image/x-icon;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=" rel="icon" type="image/x-icon">
-  </head>
-  <body>
+<!DOCTYPE html>
+<html>
+<head>
+<title>
 
-    <h1 class="center-text">
-      <!-- ↓ Change "Hey now hey now nah now!" to something else and head on back to Begin! -->
-      Hiya hiya!
-    </h1>
+&hearts; ilbt &hearts;
 
-    <p class="center-text">
-      Your <a href="https://begin.com" class="link" target="_blank">Begin</a> app is ready to go!
-    </p>
+</title>
 
-  </body>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<script src="https://use.typekit.net/iay6fpi.js"></script>
+<script>try{Typekit.load({ async: true });}catch(e){}</script>
+<link type="text/css" rel="stylesheet" href="/swirly.css">
+<link href="webfonts/ss-social-regular.css" rel="stylesheet" />
+<link href="webfonts/ss-standard.css" rel="stylesheet" />
+
+<script language="JavaScript">
+
+var links = new Array(10)
+links[0] = "index.html"
+links[1] = "index.html"
+links[2] = "paint.html"
+links[3] = "index.html"
+links[4] = "paint.html"
+links[5] = "index.html"
+links[6] = "index.html"
+links[7] = "index.html"
+links[8] = "paint.html"
+links[9] = "index.html"
+links[10] = "paint.html"
+function go() {
+var a = 1+Math.round(Math.random()*10)
+var i = a
+location = links[i]
+}
+
+</script>
+
+
+
+
+</head>
+
+<body>
+<div cl
+  <div class="type">
+I like big type &amp; I cannot lie.
+</div>
+
+
+
+<div class="footer">
+
+<div class="footer-left">
+<div class="typeface">
+Typeface: <a href="https://typekit.com/fonts/corner-store-jf">Corner Store</a> by <a href="https://www.jukeboxfonts.com/">Jukebox Fonts</a>
+</div>
+<div class="background">
+Background: <a href="https://www.youworkforthem.com/graphic/E0233/fenwick-infinityland-01">Infinity Land</a> by <a href="http://rfstudio.ca/">Ray Fenwick</a>
+</div>
+</div>
+
+<div class="footer-right">
+<div class="twitter">
+<a href="http://twitter.com/ilikebigtype" class="ss-icon ss-social-regular">twitter</a>
+
+</div><a href="javascript:go()">
+<div class="button">
+<span class="ss-icon">refresh</span> New style
+</div>
+</a>
+</div>
+
+
+</div>
+
+
+
+
+</body>
 </html>
 `
 
